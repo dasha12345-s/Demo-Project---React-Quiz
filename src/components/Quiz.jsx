4d08c08a8c07d0f92d57a032dfd873340e1c13cc
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-03-12 19:36:49 -0400

    quiz logic is added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,28 +1,77 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 import QUESTIONS from "../questions.js";
-import questions from '../questions.js';
+import quizComplited from '../assets/quiz-complete.png'
+import TimeForAnswer from './TimeForAnswer.jsx';
 
-export default function Quiz({id, text, answers}){
-
+export default function Quiz(){
+  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectAnswer(selectedAnswer){
-    setUserAnswers();
+  const activeQuestionIndex = 
+    answerState === '' ? userAnswers.length : userAnswers.length - 1;
+  const quizOver = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+    setAnswerState('answered');
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+
+    setTimeout(() => {
+      if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+        setAnswerState('correct');
+      } else {
+        setAnswerState('wrong');
+      }
+
+    setTimeout(() => {
+        setAnswerState('');
+      }, 2000);
+    }, 1000);
+  },
+   [activeQuestionIndex]);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null),
+  [handleSelectAnswer]);
+
+  if (quizOver){
+    return (
+      <div id='summary'>
+        <img src={quizComplited} alt='Trophy icon'/>
+        <h2>Quiz commplite</h2>
+      </div>
+    );
   }
 
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return(
-    <div id='question'>
+    <div id='quiz'>
+      <div id='question'>
+      <TimeForAnswer
+        key={activeQuestionIndex}
+        timeout={10000} onTimeout={handleSkipAnswer}/>
       <h2>
         {QUESTIONS[activeQuestionIndex].text}
         </h2>
       <ul id='answers'>
-      {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-        <li key={answer} className='answer'>
-          <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-        </li>
-      ))}
+      {shuffledAnswers.map((answer) => {
+        const isSelected = userAnswers[userAnswers.length - 1] === answer;
+        let cssClass = '';
+
+        if (answerState ==='answered' && isSelected){
+          cssClass = 'selected'
+        }
+
+        if ((answerState === 'correct' || answerState === 'wrong') && isSelected){cssClass = answerState;
+        }
+
+      return <li key={answer} className='answer'>
+          <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>{answer}</button>
+        </li>})}
         </ul>
+    </div>
     </div>
   )
 }
---
+++ src/components/TimeForAnswer.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function TimeForAnswer({timeout, onTimeout}){
+
+  const [remainingTime, setRemaningTime] = useState(timeout)
+
+  useEffect(() => {
+    console.log('seting TINEOUT')
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+        clearTimeout(timer);
+      };
+  }, [timeout, onTimeout]);
+  
+
+  useEffect(() => {
+    console.log('seting INTERVAL')
+    const interval = setInterval(() => {
+      setRemaningTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return() => {
+        clearInterval(interval);
+      }; 
+}, []);
+
+  return <progress id='question-time' max={timeout} value={remainingTime}/>
+}
