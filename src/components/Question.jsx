6139ba889d4905d6432da85e6e606729304fc148
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-03-13 13:56:23 -0400

    answers added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,20 +1,59 @@
+import {useState} from 'react';
 import TimeForAnswer from "./TimeForAnswer.jsx"
 import Answers from "./Answers.jsx"
+import QUESTIONS from '../questions.js'
 
-export default function Question({questionText, answers, onSelectAnswer, selectedAnswer,answerState, onSkip}){
+export default function Question({
+  keyCustom,
+  onSelectAnswer, 
+  onSkip
+}){
+  const [answer, selectAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null
+  });
+
+  function handleSelectAnswer(answer){
+    selectAnswer(
+      {
+        selectedAnswer: answer,
+        isCorrect: null
+      }
+    )
+
+    setTimeout(() => {
+      selectAnswer({
+          selectedAnswer: answer,
+          isCorrect: QUESTIONS[keyCustom].answers[0]  === answer
+        })
+
+        setTimeout(() => {
+          onSelectAnswer(answer);
+        }, 2000)
+    }, 1000)
+  }
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null){
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer){
+    answerState = 'answered'
+  }
+
   return(
     <div id='question'>
       <TimeForAnswer
         timeout={10000} 
         onTimeout={onSkip}/>
       <h2>
-        {questionText}
+        {QUESTIONS[keyCustom].text}
         </h2>
       <Answers 
-      answers={answers}  
-      selectedAnswer={selectedAnswer}
+      answers={QUESTIONS[keyCustom].answers}  
+      selectedAnswer={answer.selectedAnswer}
       answerState={answerState}
-      onSelect={onSelectAnswer}
+      onSelect={handleSelectAnswer}
       />
     </div>
   )
