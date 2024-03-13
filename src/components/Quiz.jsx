359740b2c3b007ba12fd5a23261597ae005b4d87
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-03-13 17:59:59 -0400

    last update

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import {useState, useCallback} from 'react';
 import QUESTIONS from "../questions.js";
-import quizComplited from '../assets/quiz-complete.png'
 import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 export default function Quiz(){
   const [userAnswers, setUserAnswers] = useState([]);
@@ -22,10 +22,7 @@ export default function Quiz(){
 
   if (quizOver){
     return (
-      <div id='summary'>
-        <img src={quizComplited} alt='Trophy icon'/>
-        <h2>Quiz commplite</h2>
-      </div>
+      <Summary userAnswers={userAnswers}/>
     );
   }
 
